fb02449c279ef9feff0b66e00194e052162fc2ae
Santosh Iyer <SIyer@commercehub.com>
2015-11-25 16:58:27 -0500

    tutorial script upto hooking up a data model statically

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -2,8 +2,8 @@ var CommentBox = React.createClass({
   render: function(){
     return (
       <div className="commentBox">
-      <h1> Comment Box </h1>
-        <CommentList />
+      <h1> Comments </h1>
+        <CommentList data={this.props.data} />
         <CommentForm />
     </div>
     );
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function(){
     return (
       <div className="CommentList">
-        Hello, I'm am comment List.
+        <Comment author="John Doe">This is a comment</Comment>
+        <Comment author="Jane Smith">This is the next comment</Comment>
       </div>
     );
   }
@@ -30,3 +31,27 @@ var CommentForm = React.createClass({
   }
 });
 
+// By surrounding a JavaScript expression in braces inside JSX (as either an attribute or child), you can drop text or React components into the tree.
+
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var data = [
+  {id: 1, author: "John Doe", text: "This is one comment"},
+  {id: 2, author: "Jane Smith", text: "This is another comment"}
+];
+
+ReactDOM.render(
+  <CommentBox data={data} />,
+  document.getElementById('content')
+);
